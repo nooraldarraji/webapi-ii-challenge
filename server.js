c86bdb0b5e99f0fa78788032b760733f90d68fb2
Noor Al Darraji <077068@yahoo.com>
2019-09-03 17:05:03 -0500

    Implemented GET posts and GET by post ID :hammer:

--- server.js
+++ server.js
@@ -9,4 +9,25 @@ server.get('/', (req, res) => {
     res.status(200).json({ message: 'Success!' })
 })
 
+server.get('/api/posts', (req, res) => {
+    db.find() 
+        .then(result => res.status(201).json(result))
+        .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
+})
+
+server.get('/api/posts/:id', (req, res) => {
+    const id = req.params.id
+
+    db.findById(id)
+        .then(result => {
+            console.log(result.length)
+            if (result.length > 0) {
+                res.status(201).json(result)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+})
+
+
 module.exports = server
