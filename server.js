667ae63d649d2b5230459c90b94988e9762d7c02
Noor Al Darraji <077068@yahoo.com>
2019-09-03 17:29:18 -0500

    Implemented GET posts by comment ID :hammer:

--- server.js
+++ server.js
@@ -20,13 +20,35 @@ server.get('/api/posts/:id', (req, res) => {
 
     db.findById(id)
         .then(result => {
-            console.log(result.length)
-            if (result.length > 0) {
+            console.log(result.length) // btw this console log is not working :(
+            if (result.length >= 1) {
                 res.status(201).json(result)
             } else {
                 res.status(404).json({ message: "The post with the specified ID does not exist." })
             }
         })
+})
+
+server.get('/api/posts/:id/comments', (req, res) => {
+    const postId = req.params.id
+
+    db.findById(postId)
+        .then(posts => {
+            if (posts.length >= 1) { 
+                db.findPostComments(Number(postId))
+                    .then(result => {
+                        res.status(201).json(result)
+                    })
+                    .catch(error => {
+                        res.status(500).json({ error: "The comments information could not be retrieved." })
+                    })
+                } else {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+                }
+            })
+            .catch(error => {
+                console.log('Error occured -> ', error)
+            })
 })
 
 
